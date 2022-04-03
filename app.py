53e171a8678e5a489f973acaf16bc6b380976923
student <student@ubuntu-14.edu-wsb.local>
2022-04-03 16:06:04 +0200

    poprawki

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+""" docsting """
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    """docstring"""
+    return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
 
-@app.route('/') 
-def index(): 
-    return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>' 
- 
-if __name__ == "__main__": 
-    app.run(debug=True) 
+if __name__ == "__main__":
+    app.run(debug=True)
